3ae18d2b1b8e5eb2a514b5460b3b79d9abc246ec
Behruz Valiev <behruzvaliev@gmail.com>
2020-02-26 19:01:34 +0500

    fix:  create a dir archive

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"archive/tar"
 	"archive/zip"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"sync"
 )
 
@@ -81,6 +83,7 @@ func main() {
 	// 	log.Println(err)
 	// }
 
+	creatDir()
 	UnPack("asimple43.tar.gz")
 
 	wg.Wait()
@@ -134,7 +137,7 @@ func UnTar(r io.Reader) {
 func createArchDir(path string) error {
 	if path == "dir" {
 		os.Mkdir("name_of_dir", 0600)
-		// walk to the 
+		// walk to the
 	} else {
 		os.Create("filename")
 		// than call a tar function
@@ -293,4 +296,59 @@ func ArchiveZip(filename string) error {
 	}
 
 	return err
+}
+
+func checkerror(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func creatDir() {
+
+	destinationfile := "tarball.tar"
+	sourcedir := "./"
+
+	dir, err := os.Open(sourcedir)
+	checkerror(err)
+	defer dir.Close()
+
+	// get list of files
+	files, err := dir.Readdir(0)
+	checkerror(err)
+
+	// create tar file
+	tarfile, err := os.Create(destinationfile)
+	checkerror(err)
+	defer tarfile.Close()
+
+	var fileWriter io.WriteCloser = tarfile
+
+	tarfileWriter := tar.NewWriter(fileWriter)
+	defer tarfileWriter.Close()
+
+	for _, fileInfo := range files {
+
+		if fileInfo.IsDir() {
+			continue
+		}
+
+		file, err := os.Open(dir.Name() + string(filepath.Separator) + fileInfo.Name())
+		checkerror(err)
+		defer file.Close()
+
+		// prepare the tar header
+		header := new(tar.Header)
+		header.Name = file.Name()
+		header.Size = fileInfo.Size()
+		header.Mode = int64(fileInfo.Mode())
+		header.ModTime = fileInfo.ModTime()
+
+		err = tarfileWriter.WriteHeader(header)
+		checkerror(err)
+
+		_, err = io.Copy(tarfileWriter, file)
+		checkerror(err)
+	}
 }
